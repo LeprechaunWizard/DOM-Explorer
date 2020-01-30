8b703ea64e1ca8236bba77a99747ffd4fd0ebf63
LeprechaunWizard <leprechaunmage@gmail.com>
2020-01-30 18:43:43 -0500

    added first attempt at the lights

--- scripts/popup.js
+++ scripts/popup.js
@@ -1,4 +1,16 @@
+/**
+* file that shows the content to the screen. It is responsible  for the 
+* UI of the program. 
+*/
+
 let recordDOM = document.getElementById("recordDOM");
+
+let precent = -1;
+
+let lowerBound = 50;
+let upperBound = 150;
+
+resetColors();
 
 recordDOM.addEventListener("click", element => {
   let color = element.target.value;
@@ -27,6 +39,14 @@ document.addEventListener("DOMContentLoaded", function() {
   });
 });
 
+// Tab listener 
+// https://materializecss.com/tabs.html
+var tabs = document.querySelectorAll('.tabs')
+for (var i = 0; i < tabs.length; i++){
+  M.Tabs.init(tabs[i]);
+}
+
+
 // on message received, do something iff request.DIFF exists (i.e. content.js is sending a diff)
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
   if (!sender || !sender.tab) {
@@ -41,7 +61,7 @@ chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
     console.log(request.percent);
     updatePopup(request);
     return true;
-  } 
+  }
 });
 
 // update popup.html after diff has been received
@@ -59,26 +79,88 @@ function updatePopup(request) {
 
   // update percentage
   if (request.percent > 0 && request.percent < 1) {
-    document.getElementById("diff").innerHTML =
-      Number(
-        parseFloat(
-          Math.round(request.percent.toFixed(10) * 100) / 100
-        ).toFixed(2)
-      ) + "%";
+
+    let calqulatedPrecent = Number( parseFloat( Math.round(request.percent.toFixed(10) * 100) / 100).toFixed(2));
+
+    console.log(calqulatedPrecent);
+    analysePercent(calqulatedPrecent);
+
+    document.getElementById("diff").innerHTML = calqulatedPrecent + "%";
+
   } else if (request.percent > 0 || request.percent === 0) {
     document.getElementById("diff").innerHTML =
       Number(request.percent.toFixed(2)) + "%";
   }
-  
+
   // update collapsible accordion
   if (request.DIFF.content !== undefined) {
     document.getElementById("modify-text").innerHTML = "No changes yet.";
   } else {
     console.log(request.DIFF);
     document.getElementById('modify-text').innerHTML = jsondiffpatch.formatters.html.format(request.DIFF, request.prev);
-    jsondiffpatch.formatters.html.hideUnchanged();  
+    jsondiffpatch.formatters.html.hideUnchanged();
   }
 }
+
+
+//function to reset the colors of the lights
+function resetColors() {
+  let lights = document.getElementsByClassName("light")
+
+  for(let i = 0; i < lights.length; i++) {
+    console.log(lights[i].id)
+
+    if(lights[i].id.localeCompare('redLight') === 0) {
+      lights[i].style.backgroundColor = "DarkRed";
+    }
+
+    else if(lights[i].id.localeCompare('yellowLight') === 0) {
+      lights[i].style.backgroundColor = "DarkGoldenRod";
+    }
+
+    else if(lights[i].id.localeCompare('greenLight') === 0) {
+      lights[i].style.backgroundColor = "DarkGreen";
+    }
+    
+  }
+
+}
+
+
+// function to switch which light colors are on
+function setColors(lightOn) {
+  resetColors();
+
+  switch(lightOn) {
+    case "red":
+      document.getElementById("redLight").style.backgroundColor = "red";
+      break;
+    case "yellow":
+      document.getElementById("yellowLight").style.backgroundColor = "yellow";
+      break;
+    case "green":
+      document.getElementById("greenLight").style.backgroundColor = "green";
+      break;
+    default:
+      console.log("Invalid light color");
+  }
+}
+
+// function to change color of lights based on the percentage
+function analysePercent(percent) {
+
+  console.log("Analyse Percent")
+
+  if(percent < lowerBound) {
+    setColors("green");
+  } else if(percent > upperBound) {
+    setColors("red");
+  } else {
+    setColors("yellow");
+  }
+}
+
+
 
 // return name or nodename of a diff
 function getName(diffObj) {
